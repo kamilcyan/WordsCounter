665ba1ba5799c4eadd5b0127058ffb5f31f32ddc
kamilcyan <kamil.lucjanek@wp.pl>
2020-01-31 18:58:19 +0100

    finish

---
+++ WordsCounter/WordsCounter/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WordsCounter
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CountButton_Click(object sender, RoutedEventArgs e)
+        {
+            string str = StringBox.Text;
+            int count = 0, leng = 0;
+
+            while (leng <= str.Length - 1)
+            {
+                if (str[leng] == ' ' || str[leng]=='\n' || str[leng] == '\t' || str[leng]=='.' || str[leng] == ',')
+                {
+                    count++;
+                }
+                leng++;
+            }
+            ResultBlock.Text = count.ToString();
+        }
+    }
+}
